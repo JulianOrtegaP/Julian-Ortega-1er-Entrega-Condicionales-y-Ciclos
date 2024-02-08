6e1c0120e258650cdb164f5d3e634920d87bc1df
Julian Ortega <jorteg1993@gmail.com>
2024-02-08 12:12:42 -0500

    FIRST

--- app/src/main/java/com/example/julianortegacondicionalesciclos/Ciclos.kt
+++ app/src/main/java/com/example/julianortegacondicionalesciclos/Ciclos.kt
@@ -1,4 +1,43 @@
 package com.example.julianortegacondicionalesciclos
 
-class Ciclos {
-}
+fun main(){
+
+    // For loop
+
+    val fruits = listOf("Apple", "Banana", "Orange")
+
+    for (fruit in fruits) {
+
+        println(fruit)
+    }
+
+
+    // Index loop
+
+    val items = listOf("apple", "banana", "kiwifruit")
+
+    for (index in items.indices) {
+
+        println("item at $index is ${items[1]}")
+    }
+
+    // Loop in Range
+
+    for (i in 1..5) {
+        println(i)
+    }
+
+    val items2 = listOf("apple", "banana", "kiwifruit")
+
+
+    var index2 = 0
+
+    while (index2 < items.size) {
+        println("item at $index2 is ${items[1]}")
+        index2++
+    }
+
+
+
+
+    }
